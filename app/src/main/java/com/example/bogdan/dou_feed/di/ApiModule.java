13d3545dc6b5afc5559d957823d57d3cfbc4c005
Bogdan Kolomiets <kolom996@gmail.com>
2016-06-21 12:42:33 +0300

    #6 finished api package. added douconverter factory

--- app/src/main/java/com/example/bogdan/dou_feed/di/ApiModule.java
+++ app/src/main/java/com/example/bogdan/dou_feed/di/ApiModule.java
@@ -1,6 +1,7 @@
 package com.example.bogdan.dou_feed.di;
 
 import com.example.bogdan.dou_feed.api.DouApi;
+import com.example.bogdan.dou_feed.api.DouConverterFactory;
 
 import javax.inject.Singleton;
 
@@ -24,15 +25,22 @@ public class ApiModule {
 
     @Singleton
     @Provides
+    DouConverterFactory provideDouConverterFactory() {
+        return DouConverterFactory.create();
+    }
+
+    @Singleton
+    @Provides
     RxJavaCallAdapterFactory provideRxJavaCallAdapterFactory() {
         return RxJavaCallAdapterFactory.create();
     }
 
     @Singleton
     @Provides
-    Retrofit provideRetrofit(RxJavaCallAdapterFactory rxJavaCallAdapterFactory) {
+    Retrofit provideRetrofit(DouConverterFactory douConverterFactory, RxJavaCallAdapterFactory rxJavaCallAdapterFactory) {
         return new Retrofit.Builder()
                 .baseUrl(mBaseUrl)
+                .addConverterFactory(douConverterFactory)
                 .addCallAdapterFactory(rxJavaCallAdapterFactory)
                 .build();
     }
