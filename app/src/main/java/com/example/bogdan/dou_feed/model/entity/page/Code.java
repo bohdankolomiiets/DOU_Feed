faaed4d66bd63d80bd171d15f0bf36e0851f0d1b
Bogdan Kolomiets <kolom996@gmail.com>
2016-06-26 21:03:08 +0300

    #66 Adding Code class

---
+++ app/src/main/java/com/example/bogdan/dou_feed/model/entity/page/Code.java
@@ -0,0 +1,30 @@
+package com.example.bogdan.dou_feed.model.entity.page;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.TextView;
+
+import com.example.bogdan.dou_feed.R;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 26.06.16
+ */
+public class Code extends PageElement {
+    private String mCodeText;
+
+    public Code(String codeText) {
+        mCodeText = codeText;
+    }
+
+    @Override
+    void display(LayoutInflater inflater, ViewGroup container) {
+        HorizontalScrollView codeView =
+                (HorizontalScrollView) inflater.inflate(R.layout.article_code, null);
+        TextView codeTextView = (TextView) codeView.findViewById(R.id.articleCode);
+        codeTextView.setText(mCodeText);
+        
+    }
+}
