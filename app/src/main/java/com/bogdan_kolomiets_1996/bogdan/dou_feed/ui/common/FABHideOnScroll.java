093a5863b61c6bcdda12441e6e1b7c3e7df26efd
Bogdan Kolomiets <kolom996@gmail.com>
2016-07-04 13:54:55 +0300

    #96 Adding Fab hide on Scroll

---
+++ app/src/main/java/com/bogdan_kolomiets_1996/bogdan/dou_feed/ui/common/FABHideOnScroll.java
@@ -0,0 +1,35 @@
+package com.bogdan_kolomiets_1996.bogdan.dou_feed.ui.common;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author Bogdan Kolomiets
+ * @version 1
+ * @date 04.07.16
+ */
+public class FABHideOnScroll extends FloatingActionButton.Behavior {
+
+    public FABHideOnScroll(Context context, AttributeSet attributeSet) {
+        super();
+    }
+
+    @Override
+    public void onNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, android.view.View target, int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed);
+        if(dyConsumed > 0 && child.getVisibility() == View.VISIBLE){
+            child.hide();
+        } else if(dyConsumed < 0 && child.getVisibility() == View.GONE){
+            child.show();
+        }
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout, FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL;
+    }
+}
