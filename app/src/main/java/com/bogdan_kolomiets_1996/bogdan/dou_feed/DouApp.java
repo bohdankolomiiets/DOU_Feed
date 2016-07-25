7785be42101b44065da26e61c123fb59dba76e8f
Bogdan Kolomiets <kolom996@gmail.com>
2016-07-25 14:41:42 +0300

    #107 Deleting facebook sdk

--- app/src/main/java/com/bogdan_kolomiets_1996/bogdan/dou_feed/DouApp.java
+++ app/src/main/java/com/bogdan_kolomiets_1996/bogdan/dou_feed/DouApp.java
@@ -2,16 +2,11 @@ package com.bogdan_kolomiets_1996.bogdan.dou_feed;
 
 import android.app.Application;
 import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.provider.Settings;
 
-import com.bogdan_kolomiets_1996.bogdan.dou_feed.di.module.ApiModule;
 import com.bogdan_kolomiets_1996.bogdan.dou_feed.di.component.AppComponent;
-import com.bogdan_kolomiets_1996.bogdan.dou_feed.di.module.AppModule;
 import com.bogdan_kolomiets_1996.bogdan.dou_feed.di.component.DaggerAppComponent;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import com.bogdan_kolomiets_1996.bogdan.dou_feed.di.module.ApiModule;
+import com.bogdan_kolomiets_1996.bogdan.dou_feed.di.module.AppModule;
 
 /**
  * @author Bogdan Kolomiets
@@ -25,8 +20,6 @@ public class DouApp extends Application {
   public void onCreate() {
     super.onCreate();
     resolveDependencies();
-    FacebookSdk.sdkInitialize(getApplicationContext());
-    AppEventsLogger.activateApp(this);
   }
 
   private void resolveDependencies() {
