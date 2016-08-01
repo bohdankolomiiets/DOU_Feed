51ebbbcb97d0672e6c25e1610fa738a2319ea381
Bogdan Kolomiets <kolom996@gmail.com>
2016-08-01 23:47:26 +0300

    #113 Refactoring CommentParserHelper

--- app/src/main/java/com/bogdan_kolomiets_1996/bogdan/dou_feed/api/helper/CommentParserHelper.java
+++ app/src/main/java/com/bogdan_kolomiets_1996/bogdan/dou_feed/api/helper/CommentParserHelper.java
@@ -21,6 +21,15 @@ public class CommentParserHelper {
   private static final String EMPTY_DATE = "";
   private static final String EMPTY_CONTENT = "";
 
+  private static final String ID_COMMENTS_BLOCK = "commentsList";
+
+  private static final String SELECTOR_IMAGE_URL = "img.g-avatar";
+  private static final String SELECTOR_AUTHOR_NAME = "a";
+  private static final String SELECTOR_DATE = ".comment-link";
+  private static final String SELECTOR_CONTENT = ".text.b-typo";
+
+  private static final String ATTR_SELECTOR_IMG = "src";
+
   private List<CommentItem> mComments;
 
   @Inject
@@ -30,27 +39,39 @@ public class CommentParserHelper {
 
   public List<CommentItem> parseComments(Document document) {
     Element commentBlock = selectCommentsBlock(document);
-    for (Element commentItem : commentBlock.children()) {
-      String imageUrl = selectImageUrl(commentItem);
-      String authorName = getAuthorName(commentItem);
-      String date = getTxtDate(commentItem);
-
-      Header header = new Header(imageUrl, authorName, date);
-
-      String content = getContent(commentItem);
-      CommentItem comment = new CommentItem(header, content);
-      mComments.add(comment);
+    for (Element item : commentBlock.children()) {
+      addCommentToList(item);
     }
 
     return mComments;
   }
 
   private Element selectCommentsBlock(Document document) {
-    return document.getElementById("commentsList");
+    return document.getElementById(ID_COMMENTS_BLOCK);
+  }
+
+  private void addCommentToList(Element item) {
+    CommentItem comment = createComment(item);
+    mComments.add(comment);
+  }
+
+  private CommentItem createComment(Element item) {
+    Header header = createHeader(item);
+    String content = getContent(item);
+
+    return new CommentItem(header, content);
+  }
+
+  private Header createHeader(Element item) {
+    String imageUrl = selectImageUrl(item);
+    String authorName = getAuthorName(item);
+    String date = getTxtDate(item);
+
+    return new Header(imageUrl, authorName, date);
   }
 
   private String selectImageUrl(Element commentItem) {
-    return commentItem.select("img.g-avatar").first().attr("src");
+    return commentItem.select(SELECTOR_IMAGE_URL).first().attr(ATTR_SELECTOR_IMG);
   }
 
   private String getAuthorName(Element commentItem) {
@@ -65,7 +86,7 @@ public class CommentParserHelper {
   }
 
   private String selectAuthorName(Element commentItem) {
-    return commentItem.select("a").first().text();
+    return commentItem.select(SELECTOR_AUTHOR_NAME).first().text();
   }
 
   private String getTxtDate(Element commentItem) {
@@ -80,7 +101,7 @@ public class CommentParserHelper {
   }
 
   private String selectDate(Element commentItem) {
-    return commentItem.select(".comment-link").first().text();
+    return commentItem.select(SELECTOR_DATE).first().text();
   }
 
   private String getContent(Element item) {
@@ -95,6 +116,6 @@ public class CommentParserHelper {
   }
 
   private String selectContent(Element item) {
-    return item.select(".text.b-typo").first().text();
+    return item.select(SELECTOR_CONTENT).first().text();
   }
 }
